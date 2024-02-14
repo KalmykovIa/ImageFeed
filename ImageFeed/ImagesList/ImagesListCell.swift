06bdc0fbc411360b297492095025a28d482306b6
Iaroslav Kalmykov <kalmykov-iap@yandex.ru>
2024-02-15 02:18:43 +0900

    Выполнено задания спринта 8

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Ярослав Калмыков on 12.02.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+}
